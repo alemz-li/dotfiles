9d6d6d7a5f79fc8201249b7fbc717e25202d43a3
Alejandro Meza <alemz-li@outlook.com>
2024-03-22 18:31:42 -0700

    Updated colorscheme and did some changes
    Changes to colorscheme, lualine color, indent plugin and noice

--- .config/nvim/lua/alemz/plugins/colorscheme.lua
+++ .config/nvim/lua/alemz/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("catppuccin")
-	end,
+	{
+		"bluz71/vim-moonfly-colors",
+		name = "moonfly",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme moonfly")
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+	},
 }
--- .config/nvim/lua/alemz/plugins/indent.lua
+++ .config/nvim/lua/alemz/plugins/indent.lua
@@ -3,17 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
 	config = function()
-		local highlight = {
-			"CursorColumn",
-			"Whitespace",
-		}
-		require("ibl").setup({
-			indent = { highlight = highlight, char = "" },
-			whitespace = {
-				highlight = highlight,
-				remove_blankline_trail = false,
-			},
-			scope = { enabled = false },
-		})
+		require("ibl").setup()
 	end,
 }
