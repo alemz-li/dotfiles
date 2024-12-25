b9791651ca589939a9e3721d45bb8bb95bf5d566
Alejandro Meza <alemz-li@outlook.com>
2024-12-24 22:48:46 -0800

    added undotree, gitsigns keymaps, removed lualine bg

--- nvim/.config/nvim/lua/alemz/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/alemz/plugins/gitsigns.lua
@@ -1,5 +1,79 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end, { desc = "Next hunk" })
+
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end, { desc = "Previous hunk" })
+
+				-- Actions
+				local opts = { noremap = true, silent = true }
+
+				-- Stage/Reset Hunk
+				opts.desc = "Stage hunk"
+				map("n", "<leader>hs", gitsigns.stage_hunk, opts)
+
+				opts.desc = "Reset hunk"
+				map("n", "<leader>hr", gitsigns.reset_hunk, opts)
+
+				-- Visual Mode: Stage/Reset Hunk
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Stage hunk (visual)" })
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "Reset hunk (visual)" })
+
+				-- Stage/Reset Buffer
+				map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage entire buffer" })
+				map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset entire buffer" })
+
+				-- Undo Stage Hunk
+				map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+
+				-- Preview Hunk
+				map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+
+				-- Blame
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end, { desc = "Blame line (full)" })
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle current line blame" })
+
+				-- Diff
+				map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this file" })
+				map("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end, { desc = "Diff against the last commit" })
+
+				-- Toggle Deleted
+				map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted lines" })
+
+				-- Text object for selecting a hunk
+				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
+			end,
+		})
+	end,
 }
--- nvim/.config/nvim/lua/alemz/plugins/lualine.lua
+++ nvim/.config/nvim/lua/alemz/plugins/lualine.lua
@@ -1,9 +1,44 @@
+local colors = {
+	color3 = nil,
+	color6 = "#9e9e9e",
+	color7 = "#80a0ff",
+	color8 = "#ae81ff",
+	color0 = "#1c1c1c",
+	color1 = "#ff5189",
+	color2 = "#c6c6c6",
+}
+
+local moonfly = {
+	replace = {
+		a = { fg = colors.color0, bg = colors.color1, gui = "bold" },
+		b = { fg = colors.color2, bg = colors.color3 },
+	},
+	inactive = {
+		a = { fg = colors.color6, bg = colors.color3, gui = "bold" },
+		b = { fg = colors.color6, bg = colors.color3 },
+		c = { fg = colors.color6, bg = colors.color3 },
+	},
+	normal = {
+		a = { fg = colors.color0, bg = colors.color7, gui = "bold" },
+		b = { fg = colors.color2, bg = colors.color3 },
+		c = { fg = colors.color2, bg = colors.color3 },
+	},
+	visual = {
+		a = { fg = colors.color0, bg = colors.color8, gui = "bold" },
+		b = { fg = colors.color2, bg = colors.color3 },
+	},
+	insert = {
+		a = { fg = colors.color0, bg = colors.color2, gui = "bold" },
+		b = { fg = colors.color2, bg = colors.color3 },
+	},
+}
+
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "moonfly",
+				theme = moonfly,
 			},
 			sections = {
 				lualine_b = {
